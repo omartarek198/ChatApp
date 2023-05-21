eb26481c7d1dfd3446c47756e9fdd37d2d7de786
omartarek198 <omar.tarek29@msa.edu.eg>
2023-05-21 05:32:13 +0300

    removed extra steps in algorithm

--- CRSA.py
+++ CRSA.py
@@ -2,25 +2,20 @@ import random
 
 class RSA:
     def __init__(self,msg) -> None:
-        
+            self.d = 0
+            self.e = 0
+            self.N =0
+            
             print("Hello, RSA!")
 
-            keysize = 32
-
-            e, d, N = self.generateKeys(keysize)
+            keysize = 16
+            
+            self.generateKeys(keysize)
 
        
 
-            enc = self.encrypt(e, N, msg)
-            dec = self.decrypt(d, N, enc)
-
-            print(f"Message: {msg}")
-            print(f"e: {e}")
-            print(f"d: {d}")
-            print(f"N: {N}")
-            print(f"enc: {enc}")
-            print(f"dec: {dec}")
-    def generateKeys(self,keysize=1024):
+          
+    def generateKeys(self,keysize=8 ):
         e = d = N = 0
 
         # get prime nums, p & q
@@ -35,16 +30,16 @@ class RSA:
 
         # choose e
         # e is coprime with phiN & 1 < e <= phiN
-        while True:
-            e = random.randrange(2 ** (keysize - 1), 2 ** keysize - 1)
-            if (self.isCoPrime(e, phiN)):
+        for i in range(2,phiN):
+            if self.gcd(i,phiN) == 1:
+                e = i
                 break
 
-        # choose d
-        # d is mod inv of e with respect to phiN, e * d (mod phiN) = 1
+        # e * d (mod phiN) = 1
         d = self.modularInv(e, phiN)
-
-        return e, d, N
+        self.e = e
+        self.d = d
+        self.N = N
     def isPrime(self,n):
         """
             return True if n prime
@@ -56,7 +51,19 @@ class RSA:
             return False
 
         # low prime numbers to save time
-        lowPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+        lowPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 
+                     47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+                     101, 103, 107, 109, 113, 127, 131, 137, 139, 149,
+                     151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 
+                     199, 211, 223, 227, 229, 233, 239, 241, 251, 257
+                     , 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317
+                     , 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 
+                     401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 
+                     487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 
+                     593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 
+                     683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
+                     809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911,
+                     919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
 
         # if in lowPrimes
         if n in lowPrimes:
@@ -67,36 +74,11 @@ class RSA:
             if n % prime == 0:
                 return False
         
-        # find number c such that c * 2 ^ r = n - 1
-        c = n - 1 # c even bc n not divisible by 2
-        while c % 2 == 0:
-            c /= 2 # make c odd
-
-        # prove not prime 128 times
-        for i in range(128):
-            if not self.rabinMiller(n, c):
-                return False
+     
 
         return True
     
-    def rabinMiller(self,n, d):
-        a = random.randint(2, (n - 2) - 2)
-        x = pow(a, int(d), n) # a^d%n
-        if x == 1 or x == n - 1:
-            return True
-
-        # square x
-        while d != n - 1:
-            x = pow(x, 2, n)
-            d *= 2
-
-            if x == 1:
-                return False
-            elif x == n - 1:
-                return True
-        
-        # is not prime
-        return False
+  
 
 
     def generateLargePrime(self,keysize):
@@ -116,13 +98,7 @@ class RSA:
         while q:
             p, q = q, p % q
         return p
-    def isCoPrime(self,p, q):
-        """
-            return True if gcd(p, q) is 1
-            relatively prime
-        """
 
-        return self.gcd(p, q) == 1
 
     def egcd(self,a, b):
         s = 0; old_s = 1
@@ -167,7 +143,16 @@ class RSA:
 def main():
     msg = "123omar456"
     test = RSA(msg)
-main()
-
+    enc = test.encrypt(test.e, test.N, msg)
+    dec = test.decrypt(test.d, test.N, enc)
+    print(f"Message: {msg}")
+    print(f"e: {test.e}")
+    print(f"d: {test.d}")
+    print(f"N: {test.N}")
+    print(f"enc: {enc}")
+    print(f"dec: {dec}")
+    
+if __name__ == "__main__":
+    main()
 
 
